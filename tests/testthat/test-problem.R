59fa35cccbee6ae60ebc900fd76e676b704d0153
cebarboza <c.barbozagutierrez@erasmusmc.nl>
2025-01-30 09:56:37 +0100

    update codes

--- tests/testthat/test-problem.R
+++ tests/testthat/test-problem.R
@@ -1,4 +1,9 @@
 test_that("Cohort Counts", {
+  # Codes to work with
+  # procedure_concept_id for mechanical ventilation 4097246
+  # condition_concept_id for covid 37311061
+  # visit_detail_concept_id for intensive care 32037
+  
 
 })
 
